e6b363eda263e82b180f6f0dd3d498a0d5ad8b0e
Just-A-Mango <horace.hoff@gmail.com>
2022-03-28 17:55:08 +0200

    Removed the Python version

--- nanopy.cpp
+++ nanopy.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    string file_name,text;
-    fstream file;
-    file.open(file_name+".txt");
-    cout<<"enter file name here:";
+    string file_name;
+    cout<<"Enter file name: ";
     cin>>file_name;
-    cout<<"enter text here:";
-    cin>>text;
-    file<<text;
+    std::ifstream input(file_name);
+    for( std::string line; getline( input, line ); )
+    {
+        cout<<line<<"\n";
+    }
+    cout << "\033[F";
+    cout << "\033[F";
+    std::string s;
+    std::getline(std::cin, s, '$');
+    std::cout << s << std::endl;
     return 0;
 }
